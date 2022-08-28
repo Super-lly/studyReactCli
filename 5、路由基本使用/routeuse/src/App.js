acf566e0a5eeb293683bf332263629b1ddc4759c
Super-lly <843182286@qq.com>
2022-08-28 14:32:03 +0800

    NavLink

--- 5、路由基本使用/routeuse/src/App.js
+++ 5、路由基本使用/routeuse/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import {Link,Route} from 'react-router-dom'
+import {NavLink,Route} from 'react-router-dom'
 
 import About from './Components/About'
 import Home from './Components/Home'
@@ -18,8 +18,9 @@ export default class App extends Component {
         <div className="row">
           <div className="col-xs-2 col-xs-offset-2">
             <div className="list-group">
-              <Link className="list-group-item" to="/about">About</Link>
-              <Link className="list-group-item" to="/home">Home</Link>
+              {/* NavLink 用于添加点击连接时的展示效果 使用activeClassName为连接添加自定义效果 默认名为active */}
+              <NavLink activeClassName="istrue" className="list-group-item" to="/about">About</NavLink>
+              <NavLink activeClassName="istrue" className="list-group-item" to="/home">Home</NavLink>
             </div>
           </div>
           <div className="col-xs-6">
