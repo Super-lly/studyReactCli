b15e9e43da0a67b69c885a8a748d53a04137bd55
Super-lly <843182286@qq.com>
2022-09-06 15:37:20 +0800

    使用Provider实现redux页面刷新

--- 12、求和案例——React-redux/getcountbyrr/src/index.js
+++ 12、求和案例——React-redux/getcountbyrr/src/index.js
@@ -1,12 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {BrowserRouter} from 'react-router-dom'
+import {Provider} from 'react-redux'
 import store from './redux/store'
 import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<BrowserRouter><App/></BrowserRouter>)
-// 实现数据更新后的页面刷新
-store.subscribe(()=>{
-  root.render(<BrowserRouter><App/></BrowserRouter>)
-})
+root.render(<Provider store={store}><BrowserRouter><App/></BrowserRouter></Provider>)
